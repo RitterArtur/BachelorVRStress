ad4f8e4afcf90e675b7784a4e06b775b0e5a6cbb
RitterArtur <43034106+RitterArtur@users.noreply.github.com>
2019-12-17 18:34:17 +0100

    camera and small changes to the scene

--- VR Test/Assets/Scripts/cameraScript.cs
+++ VR Test/Assets/Scripts/cameraScript.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class cameraScript : MonoBehaviour
 {
-    public float speed = .5f;
+    public float speed = 0.5f;
+    public float CameraSpeed = 2.0f;
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +19,45 @@ public class cameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += speed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            transform.Rotate(0, -1, 0);
+        }
+        // Rotate right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        // Strafe left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        // Move forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        // Move backward
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        // Strafe right     
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             transform.position += speed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
         }
+
+        yaw += CameraSpeed * Input.GetAxis("Mouse X");
+
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
     }
 }
